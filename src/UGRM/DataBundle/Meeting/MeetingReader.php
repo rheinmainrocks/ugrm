b0f3d60eaf0d906c4e0459573d6f9be5f494146f
Markus Tacker <m@coderbyheart.de>
2014-05-08 15:42:05 +0200

    Handle minimal iCal Feeds. Fixes coderbyheart/ugrm-data#91.

--- src/UGRM/DataBundle/Meeting/MeetingReader.php
+++ src/UGRM/DataBundle/Meeting/MeetingReader.php
@@ -12,15 +12,18 @@ class MeetingReader
     public static function fetchMeetings(Usergroup $usergroup, \SplFileInfo $cacheFile)
     {
         $parser = new \CalendarParser($cacheFile);
-        $now = new \DateTime();
         foreach ($parser->getEvents() as $event) {
             $meeting = new Meeting();
             $meeting->usergroup = $usergroup;
             $meeting->name = $event->SUMMARY;
-            $meeting->description = $event->DESCRIPTION;
+            if (property_exists($event, 'DESCRIPTION')) {
+                $meeting->description = $event->DESCRIPTION;
+            }
             $meeting->time = new Carbon($event->DTSTART);
-            $meeting->location = new SimpleLocation();
-            $meeting->location->description = $event->LOCATION;
+            if (property_exists($event, 'LOCATION')) {
+                $meeting->location = new SimpleLocation();
+                $meeting->location->description = $event->LOCATION;
+            }
             $usergroup->meetings[] = $meeting;
         }
     }
